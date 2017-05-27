bf78721037bf26303f7962256f1fc4ffeca70336
ghafran <ghafranabbas@gmail.com>
2017-05-27 14:12:36 -0400

    backup

--- windows/PTZTest/Program.cs
+++ windows/PTZTest/Program.cs
@@ -13,9 +13,10 @@ namespace PTZTest
         {
             var device = PTZ.Device.GetDevice(ConfigurationManager.AppSettings["DeviceName"]);
 
+            Console.WriteLine("Capabilities: Absolute Zoom: {0}, Relative Zoom: {1}, Absolute PanTilt: {2}, Relative PanTilt: {3}", device.SupportsAbsoluteZoom, device.SupportsRelativeZoom, device.SupportsAbsolutePanTilt, device.SupportsRelativePanTilt);
             while (true)
             {
-                Console.WriteLine("Enter Command: (q, za)");
+                Console.WriteLine("Enter Command: (q, za, pa, ta)");
                 string command = Console.ReadLine();
                 if (command == "q") {
                     break;
@@ -24,7 +25,7 @@ namespace PTZTest
                 if (command == "za")
                 {
                     int currentZoom = device.GetZoom();
-                    Console.WriteLine("Current Zoom: %d, Min: %d, Max: %d, Step: %d, Default: %d", currentZoom, device.ZoomMin, device.ZoomMax, device.ZoomStep, device.ZoomDefault);
+                    Console.WriteLine("Current Zoom: {0}, Min: {1}, Max: {2}, Step: {3}, Default: {4}", currentZoom, device.ZoomMin, device.ZoomMax, device.ZoomStep, device.ZoomDefault);
                     string value = Console.ReadLine();
                     int newvalue = int.Parse(value);
                     int result = device.AbsoluteZoom(newvalue);
@@ -34,6 +35,40 @@ namespace PTZTest
                     } else
                     {
                         Console.WriteLine("Failed! %d", result);
+                    }
+                }
+
+                if (command == "pa")
+                {
+                    int currentPan = device.GetPan();
+                    Console.WriteLine("Current Pan: {0}, Min: {1}, Max: {2}, Step: {3}, Default: {4}", currentPan, device.PanMin, device.PanMax, device.PanStep, device.PanDefault);
+                    string value = Console.ReadLine();
+                    int newvalue = int.Parse(value);
+                    int result = device.AbsolutePan(newvalue);
+                    if (result == 0)
+                    {
+                        Console.WriteLine("Set Pan Successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed! {0}", result);
+                    }
+                }
+
+                if (command == "ta")
+                {
+                    int current = device.GetTilt();
+                    Console.WriteLine("Current Tilt: {0}, Min: {1}, Max: {2}, Step: {3}, Default: {4}", current, device.TiltMin, device.TiltMax, device.TiltStep, device.TiltDefault);
+                    string value = Console.ReadLine();
+                    int newvalue = int.Parse(value);
+                    int result = device.AbsolutePan(newvalue);
+                    if (result == 0)
+                    {
+                        Console.WriteLine("Set Tilt Successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed! {0}", result);
                     }
                 }
             }
